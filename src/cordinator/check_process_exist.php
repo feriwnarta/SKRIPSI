eb44a0f0e58660b0e497ac475f636a479126e0d8
feriwnarta <feriwnarta26@gmail.com>
2023-01-25 16:02:56 +0700

    update cord

--- src/cordinator/check_process_exist.php
+++ src/cordinator/check_process_exist.php
@@ -4,13 +4,37 @@ $db = new db();
 $json = file_get_contents('php://input');
 $obj = json_decode($json, true);
 
-if(!empty($obj['id_report']) && !empty($obj['id_estate_cordinator'])) {
+if (!empty($obj['id_report']) && !empty($obj['id_estate_cordinator'])) {
 	$id_estate_cordinator =  mysqli_real_escape_string($db->query, ($obj['id_estate_cordinator']));
 	$id_report = mysqli_real_escape_string($db->query, ($obj['id_report']));
-	$result = $db->select('tb_process_report', 'id_report="'.$id_report.'" && id_estate_cordinator="'.$id_estate_cordinator.'"', 'id_process_report', 'ASC');
-	if(mysqli_num_rows($result) > 0) {
-		echo json_encode('EXIST');
+	// $result = $db->select('tb_process_report', 'id_report="' . $id_report . '" && id_estate_cordinator="' . $id_estate_cordinator . '"', 'id_process_report', 'ASC');
+	$result = $db->select('tb_report', 'id_report = "' . $id_report . '" AND status = "Diproses" OR status = "Diterima"', 'id_report', "DESC");
+
+	$data_user = mysqli_fetch_assoc($result);
+
+
+	if (mysqli_num_rows($result) > 0) {
+
+		$data_proses = $db->select('tb_process_report', 'id_report = "' . $id_report . '"', 'id_report', 'DESC');
+		if (mysqli_num_rows($data_proses) > 1) {
+			echo json_encode(
+				array(
+					'status' => 'BEEN PROCESSED',
+					'data' => $data_user,
+				)
+			);
+		} else {
+			echo json_encode(array(
+				'status' => 'EXIST',
+				'data' => [],
+			));
+		}
 	} else {
-		echo json_encode('NO EXIST');
+		echo json_encode(
+			array(
+				'status' => 'NOT EXIST',
+				'data' => [],
+			)
+		);
 	}
 }
--- src/cordinator/finish_work_report.php
+++ src/cordinator/finish_work_report.php
@@ -4,66 +4,11 @@ $db = new db();
 $json = file_get_contents('php://input');
 $obj = json_decode($json, true);
 
-if (!empty($_POST['id_report']) && !empty($_POST['id_estate_cordinator'])  && !empty($_POST['message'])) {
-	$id_report = mysqli_real_escape_string($db->query, $_POST['id_report']);
-	$id_estate_cordinator = mysqli_real_escape_string($db->query, $_POST['id_estate_cordinator']);
-	$message = mysqli_real_escape_string($db->query, $_POST['message']);
-
-	if (isset($_FILES['image1'])) {
-		$image1 = $_FILES['image1'];
-		$tmp_name = $_FILES['image1']['tmp_name'];
-		$valid_ext = array('png', 'jpeg', 'jpg');
-		// file extension
-		$file_extension = pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION);
-		$file_extension = strtolower($file_extension);
-		if (in_array($file_extension, $valid_ext)) {
-			$bytes = random_bytes(20);
-			$image_location = '../../imagecordinatorwork/' . bin2hex($bytes) . ".$file_extension";
-			move_uploaded_file($tmp_name, $image_location);
-			$ubah1 = str_replace("../", "", $image_location);
-		}
-	}
-	if (isset($_FILES['image2'])) {
-		$image2 = $_FILES['image2'];
-		$tmp_name = $_FILES['image2']['tmp_name'];
-		$valid_ext = array('png', 'jpeg', 'jpg');
-		// file extension
-		$file_extension = pathinfo($_FILES['image2']['name'], PATHINFO_EXTENSION);
-		$file_extension = strtolower($file_extension);
-		if (in_array($file_extension, $valid_ext)) {
-			$bytes = random_bytes(20);
-			$image_location = '../../imagecordinatorwork/' . bin2hex($bytes) . ".$file_extension";
-			move_uploaded_file($tmp_name, $image_location);
-			$ubah2 = str_replace("../", "", $image_location);
-		}
-	}
-
-
-	if (isset($ubah1) && isset($ubah2)) {
-		// $result = $db->insert('tb_process_work_cordinator', 'id_report="' . $id_report . '", id_estate_cordinator="' . $id_estate_cordinator . '",photo_work_1="' . $ubah1 . '",photo_work_2="' . $ubah2 . '"');
-		// $status = 'process';
-
-		// $db->update('tb_process_workd_cordinator', 'photo_complete_1 = "' . $ubah1 . '", photo_complete_2 = "' . $ubah2 . '"');
-
-		$r = $db->update('tb_report', 'status="' . $status . '"', 'id_report="' . $id_report . '"');
-		echo json_encode('OKE', JSON_PRETTY_PRINT);
-	} else if (isset($ubah1)) {
-		$result = $db->insert('tb_process_work_cordinator', 'id_report="' . $id_report . '", id_estate_cordinator="' . $id_estate_cordinator . '",photo_work_1="' . $ubah1 . '"');
-		$status = 'process';
-
-		$r = $db->update('tb_report', 'status="' . $status . '"', 'id_report="' . $id_report . '"');
-		echo json_encode('OKE', JSON_PRETTY_PRINT);
-	} else if (isset($ubah2)) {
-		$result = $db->insert('tb_process_work_cordinator', 'id_report="' . $id_report . '", id_estate_cordinator="' . $id_estate_cordinator . '",photo_work_2="' . $ubah2 . '"');
-		$status = 'process';
-		$r = $db->update('tb_report', 'status="' . $status . '"', 'id_report="' . $id_report . '"');
-		echo json_encode('OKE', JSON_PRETTY_PRINT);
-	}
-	$rs = $db->insert('tb_process_report', 'id_report="' . $id_report . '", id_estate_cordinator="' . $id_estate_cordinator . '",status_process="' . $_POST['message'] . '"');
-
-
-
-	// $rs = $db->select('tb_process_work_cordinator', 'id_estate_cordinator="' . $id_estate_cordinator . '"&& id_report="' . $id_report . '"', 'id_process_work', 'DESC');
+if(!empty($obj['id_report']) && !empty($obj['id_estate_cordinator']) ) {
+	$id_report = mysqli_real_escape_string($db->query,$obj['id_report']);
+	$id_estate_cordinator = mysqli_real_escape_string($db->query,$obj['id_estate_cordinator']);
+	
+	$rs = $db->select('tb_process_work_cordinator', 'id_estate_cordinator="'.$id_estate_cordinator.'"&& id_report="'.$id_report.'"','id_process_work', 'DESC');
 
 	$data = mysqli_fetch_assoc($rs);
 	echo json_encode($data, JSON_PRETTY_PRINT);
